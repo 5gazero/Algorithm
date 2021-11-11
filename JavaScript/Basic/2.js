daac43910d2438d6e83ff6214015f06554f7a7c1
5gazero <kayo9410@gmail.com>
2021-11-11 23:27:45 +0900

    add Q2

---
+++ JavaScript/Basic/2.js
@@ -0,0 +1,9 @@
+const solution = (a, b, c) => {
+  let result = "YES", max;
+  let tot = a + b + c;
+  if (a > b) max = a;
+  else max = b;
+  if (c > max) max = c;
+  if (tot - max <= max) result = "NO";
+  return result;
+}
