0ac49aed427b176f3ebc1f9c7a944115efb9ef41
5gazero <kayo9410@gmail.com>
2021-11-13 19:19:01 +0900

    mod Q1

--- JavaScript/Basic/1.js
+++ JavaScript/Basic/1.js
@@ -1,6 +1,6 @@
 // 세 수 중 최소값
 
-function solution(a, b, c){
+const solution = (a, b, c) => {
   let result;
   if (a > b) result = b;
   else result = a;
@@ -8,5 +8,3 @@ function solution(a, b, c){
   else result = result;
   return result;
 }
-
-console.log(solution(2, 5, 1));
