4d792f8126f8a3d03063655aeb057bb71b738cd6
5gazero <kayo9410@gmail.com>
2021-11-15 21:23:08 +0900

    add Q6

---
+++ JavaScript/Basic/6.js
@@ -0,0 +1,16 @@
+// 홀수
+
+const solution = (arr) => {
+  let result = [];
+  let sum = 0,
+      min = 1000;
+  for (let i of arr) {
+    if (i % 2 === 1) {
+      sum += i;
+      if (i < min) min = i;
+    }
+    result.push(sum);
+    result.push(min);
+    return result;
+  }
+};
