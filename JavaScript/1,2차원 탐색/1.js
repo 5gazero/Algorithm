d1e5c13fcda67a54e7fc0c57bbb65f065b3ac00f
5gazero <kayo9410@gmail.com>
2021-12-05 00:12:07 +0900

    add Q1

---
+++ JavaScript/1,2차원 탐색/1.js
@@ -0,0 +1,10 @@
+// 큰 수 출력하기
+
+const solution = (arr) => {
+  let answer = [];
+  answer.push(arr[0]);
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > arr[i - 1]) answer.push(arr[i]);
+  }
+  return answer;
+};
