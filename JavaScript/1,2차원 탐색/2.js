4d4790840303c3b6ebd8f0c853173cab6e6c8271
5gazero <kayo9410@gmail.com>
2021-12-06 22:50:01 +0900

    add Q2

---
+++ JavaScript/1,2차원 탐색/2.js
@@ -0,0 +1,13 @@
+// 보이는 학생
+
+const solution = (arr) => {
+  let answer = 1;
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      answer++;
+      max = arr[i];
+    }
+  }
+  return answer;
+};
