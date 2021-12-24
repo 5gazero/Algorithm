7d6a88d57502f469a5f37eaa029087bafd361f30
5gazero <kayo9410@gmail.com>
2021-12-25 00:18:59 +0900

    add Q20

---
+++ Programmers/Level1/20.js
@@ -0,0 +1,8 @@
+const solution = (num) => {
+    let count = 0;
+    while (num !== 1 && count !== 500) {
+        (num % 2 === 1) ?num = (num * 3) + 1 : num = num / 2;
+        count++;
+    }
+    return num === 1 ? count : -1
+}
