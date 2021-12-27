3dcf083432f47f9edb5ad7acfe23d6985506fc19
5gazero <kayo9410@gmail.com>
2021-12-27 23:43:22 +0900

    mod Q19

--- Programmers/Level1/19.js
+++ Programmers/Level1/19.js
@@ -1,5 +1,7 @@
+// 최대공약수와 최소공배수
+
 const solution = (n, m) => {
-    const gcd = (a, b) => a % b === 0 ? b : gcd(b, a % b);
-    const lcm = (a, b) => a * b / gcd(a, b);
-    return [gcd(n, m), lcm(n, m)];
-}
+  const gcd = (a, b) => (a % b === 0 ? b : gcd(b, a % b));
+  const lcm = (a, b) => (a * b) / gcd(a, b);
+  return [gcd(n, m), lcm(n, m)];
+};
