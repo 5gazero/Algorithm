78b23a70a78c8084b5532cb5e92ea03d7a158e2e
5gazero <kayo9410@gmail.com>
2021-12-27 23:42:34 +0900

    mod Q18

--- Programmers/Level1/18.js
+++ Programmers/Level1/18.js
@@ -1,9 +1,11 @@
+// 제일 작은 수 제거하기
+
 const solution = (arr) => {
-  if (arr.length <= 1) return [-1]
- 
+  if (arr.length <= 1) return [-1];
+
   let min = Math.min(...arr);
   let idx = arr.indexOf(min);
- 
+
   arr.splice(idx, 1);
   return arr;
-}
+};
