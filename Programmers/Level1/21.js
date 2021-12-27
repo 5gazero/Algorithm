586d654766162309219b5938647f1b7171f060fe
5gazero <kayo9410@gmail.com>
2021-12-27 23:44:25 +0900

    mod Q21

--- Programmers/Level1/21.js
+++ Programmers/Level1/21.js
@@ -1,8 +1,10 @@
+// 자연수 뒤집어 배열로 만들기
+
 const solution = (n) => {
   let answer = [];
   do {
-      answer.push(n % 10);
-      n = Math.floor(n / 10)
-  } while (n > 0) 
+    answer.push(n % 10);
+    n = Math.floor(n / 10);
+  } while (n > 0);
   return answer;
-}
+};
