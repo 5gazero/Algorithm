d7942a6a9c940d9dbb49d092fa7985d6ed1a3266
5gazero <kayo9410@gmail.com>
2022-01-31 20:42:28 +0900

    mod Q34

--- Programmers/Level1/34.js
+++ Programmers/Level1/34.js
@@ -1,1 +1,10 @@
 // K번째수
+
+const solution = (array, commands) => {
+  const answer = [];
+  commands.map(arr => {
+    const parts = array.slice(arr[0] - 1, arr[1]).sort((a, b) => a - b);
+    answer.push(parts[arr[2] - 1]);
+  });
+  return answer;
+};
