16529783f39e647dc6380d28d5a5ae3cf4af557d
5gazero <kayo9410@gmail.com>
2022-03-29 23:15:30 +0900

    mod Q37

--- Programmers/Level1/37.js
+++ Programmers/Level1/37.js
@@ -1,1 +1,14 @@
-//
+// 약수의 개수와 덧셈
+
+const solution = (left, right) => {
+  let answer = 0;
+
+  for (let i = left; i <= right; i++) {
+    let count = 0;
+    for (let j = 1; j <= i; j++) {
+      if (i % j === 0) count++;
+    }
+    count % 2 === 0 ? (answer += i) : (answer -= i);
+  }
+  return answer;
+};
